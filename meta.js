df64fc149126d43668755a3c2b3ed3a9a184caf0
SHEN <>
2019-01-21 17:20:14 +0800

    bug fixed

--- meta.js
+++ meta.js
@@ -27,16 +27,6 @@ module.exports = {
     }
   },
   "filters": {
-    ".eslintrc.js": "lint",
-    ".eslintignore": "lint",
-    "config/test.env.js": "unit || e2e",
-    "test/unit/**/*": "unit",
-    "build/webpack.test.conf.js": "unit",
-    "test/e2e/**/*": "e2e",
-    "src/store/**/*": "vuex",
-    "src/router/**/*": "router",
-    "src/css/min.scss": "isMobile",
-    "static/js/lib-flexible.js": "isMobile",
   },
   "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
 };
