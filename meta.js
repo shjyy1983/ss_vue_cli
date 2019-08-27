4ba79aea21408af2998afdc870ae76ae161f7b93
SHEN <>
2019-08-27 22:31:51 +0800

    test

--- meta.js
+++ meta.js
@@ -34,7 +34,8 @@ module.exports = {
   },
   "filters": {
     "src/store/**/*": "vuex",
-    "src/util/**/*": "vuex"
+    "src/util/**/*": "vuex",
+    "NOTE": "vuex"
   },
   "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
 };
